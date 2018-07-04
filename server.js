a03feeae939d5fab60f09879b8b598d494d83b11
Tammy Tian <tiantammy@outlook.com>
2018-07-04 22:12:51 +0200

    added apollo engine

--- server.js
+++ server.js
@@ -2,16 +2,39 @@ import express from 'express';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import bodyParser from 'body-parser';
 import schema from './data/schema';
+import compression from 'compression';
+import { ApolloEngine } from 'apollo-engine';
+
+const app = express();
+
+
+app.use(compression());
+app.use(
+  '/graphql', 
+  bodyParser.json(), 
+  graphqlExpress({
+     schema, 
+     tracing: true 
+    })
+  );
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+
 
 const GRAPHQL_PORT = 3000;
+const ENGINE_API_KEY = 'service:tammyztian:lU6T7mfli9wNLDUKK-GgAQ';
 
-const graphQLServer = express();
+const engine = new ApolloEngine({
+    apiKey: ENGINE_API_KEY,
+})
 
-graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
-graphQLServer.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+engine.listen({
+  port: GRAPHQL_PORT,
+  expressApp: app,
+});
 
-graphQLServer.listen(GRAPHQL_PORT, () =>
-  console.log(
-    `GraphiQL is now running on http://localhost:${GRAPHQL_PORT}/graphiql`
-  )
-);
+
+// app.listen(GRAPHQL_PORT, () =>
+//   console.log(
+//     `GraphiQL is now running on http://localhost:${GRAPHQL_PORT}/graphiql`
+//   )
+// );
